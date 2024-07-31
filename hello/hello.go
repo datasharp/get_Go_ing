829f9ebe8c891c024371a4affa5d9c4591b1dc58
datasharp <krs.sharp00@gmail.com>
2024-07-30 23:28:14 -0700

    added error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,21 @@ package main
 
 import (
     "fmt"
-
+    "log"
     "example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    // set logger properties
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // request greeting message
+    message, err := greetings.Hello("")
+    if err != nil {
+        log.Fatal(err) // fatal: print error and stop function
+    }
+    // no error, then print
     fmt.Println(message)
 
     message = greetings.Goodbye("Gladys")
